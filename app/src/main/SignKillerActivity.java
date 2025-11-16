5efaad6935aa58500163f9eafc0425b1578e0796
youcefkrm07 <youcefkrm07@gmail.com>
2025-11-16 11:33:39 +0000

    Upload app/src/main/SignKillerActivity.java

---
+++ app/src/main/SignKillerActivity.java
@@ -0,0 +1,15 @@
+package com.aline.studioapp.general;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aline.studioapp.R;
+
+public class SignKillerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_killer);
+        setTitle("Sign Killer");
+    }
+}
